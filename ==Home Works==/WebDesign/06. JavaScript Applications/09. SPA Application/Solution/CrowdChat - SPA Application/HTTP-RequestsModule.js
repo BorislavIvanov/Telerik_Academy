1dae154d35fabb5b1375b2102d54c2ed4da12571
BorislavIvanov <borislav.a.ivanov@gmail.com>
2014-07-27 15:21:37 +0300

    Added Homework JS Application - SPA Application

---
+++ ==Home Works==/WebDesign/06. JavaScript Applications/09. SPA Application/Solution/CrowdChat - SPA Application/HTTP-RequestsModule.js
@@ -0,0 +1,46 @@
+﻿/// <reference path="Libraries/Q-JS.js" />
+
+var httpRequestModule = (function () {
+    function getJSON(requestUrl) {
+        var deferred = Q.defer();
+
+        $.ajax({
+            url: requestUrl,
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                deferred.resolve(data);
+            },
+            error: function (err) {
+                deferred.reject(err);
+            }
+        });
+
+        return deferred.promise;
+    }
+
+    function postJSON(requestUrl, data) {
+        var deferred = Q.defer();
+
+        $.ajax({
+            url: requestUrl,
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            dataType: "json",
+            success: function (data) {
+                deferred.resolve(data);
+            },
+            error: function (err) {
+                deferred.reject(err);
+            }
+        });
+
+        return deferred.promise;
+    }
+
+    return {
+        getJSON: getJSON,
+        postJSON: postJSON
+    }
+})();
