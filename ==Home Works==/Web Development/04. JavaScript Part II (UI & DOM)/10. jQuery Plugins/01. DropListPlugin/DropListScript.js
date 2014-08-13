bebf5ab14a7b7cf9ea2e7c4457cb7e26ff1b7c3a
BorislavIvanov <borislav.a.ivanov@gmail.com>
2014-08-13 11:35:09 +0300

    Modifications

---
+++ ==Home Works==/Web Development/04. JavaScript Part II (UI & DOM)/10. jQuery Plugins/01. DropListPlugin/DropListScript.js
@@ -0,0 +1,38 @@
+﻿(function ($) {
+    $.fn.dropdownListPlugin = function(){
+        var $this = $(this);
+        $this.hide();
+
+        $dropdownContainer = $('<div></div>').addClass('dropdown-list-container');
+        $dropdownOptions = $('<ul></ul>').addClass('dropdown-list-options');
+        
+        $options = $this.find('option');
+
+        for (var i = 0; i < $options.length; i++) {
+            $dropdownOption = $('<li></li>').addClass('dropdown-list-option')
+                                            .attr("data-value", i)
+                                            .text($options[i].innerHTML);
+            $dropdownOptions.append($dropdownOption);
+        }
+
+        $dropdownContainer.append($dropdownOptions);
+        $('body').append($dropdownContainer);
+
+        $('.dropdown-list-option').on('click', function (ev) {
+            $eventTarget = $(ev.target);
+            if ($eventTarget.attr('selected') == 'selected') {
+                $eventTarget.removeAttr('selected', '')
+
+                alert($eventTarget.text() + " is Unselected :)");
+            }
+            else {
+                $('.dropdown-list-option').removeAttr('selected', '')
+                $eventTarget.attr('selected', 'selected');
+                
+                alert($eventTarget.text() + " is Selected :)");
+            }
+        })
+    };
+}(jQuery));
+
+$('#dropdown').dropdownListPlugin();
