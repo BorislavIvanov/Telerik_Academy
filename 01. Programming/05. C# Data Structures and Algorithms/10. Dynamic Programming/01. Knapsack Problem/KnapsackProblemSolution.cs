4f86d13009941b316867dac454d5ce3ee57afd0e
BorislavIvanov <borislav.a.ivanov@gmail.com>
2014-09-29 00:16:54 +0300

    Added New DSA Homework - Dynamic Programming

---
+++ 01. Programming/05. C# Data Structures and Algorithms/10. Dynamic Programming/01. Knapsack Problem/KnapsackProblemSolution.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class KnapsackProblemSolution
+{
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Knapsack Problem Solution :)");
+        Console.WriteLine();
+        Console.ResetColor();
+
+        // Checking input size of the knapsack.
+        string consoleInputKnapsackSize = default(string);
+        int knapsackSize = default(int);
+        ValidateTheInputKnapsackSize(ref consoleInputKnapsackSize, ref knapsackSize);
+
+        // Checking input count of products.
+        string consoleInputProductsCount = default(string);
+        int productsCount = default(int);
+        ValidateTheInputProductsCount(ref consoleInputProductsCount, ref productsCount);
+
+        Console.WriteLine();
+        Console.WriteLine("Example for product adding...");
+        Console.WriteLine("beer–3-2 => name=beer – weight=3 - cost=2");
+        Console.WriteLine();
+
+        // In this dictionary will be added to all products.
+        Dictionary<string, List<int>> allProducts = new Dictionary<string, List<int>>();
+
+        // This for loop will get all inputed products from console.
+        GetAllInputedProductsFromConsole(productsCount, allProducts);
+
+        // Verify that only one product is added and return it as a result.
+        if (allProducts.Count == 1)
+        {
+            string firstProductName = allProducts.ElementAt(0).Key;
+            int firstProductWeight = allProducts.ElementAt(0).Value[0];
+            int firstProductCost = allProducts.ElementAt(0).Value[1];
+
+            Console.WriteLine("Best solution is: Name=" + firstProductName + 
+                " - Weight=" + firstProductWeight + " - Cost=" + firstProductCost);
+
+            return;
+        }
+
+        // Arrays for dynamic programming solution of the problem.
+        int[,] valuesArray = new int[productsCount + 1, knapsackSize + 1];
+        int[,] keepArray = new int[productsCount + 1, knapsackSize + 1];
+
+        InputTheProductsDataIntoArrays(allProducts, valuesArray, keepArray);
+
+        List<string> bestSolutionItems = new List<string>();
+
+        FindBestSolutionItems(knapsackSize, allProducts, keepArray, bestSolutionItems);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine();
+        Console.Write("Best choice is: ");
+        Console.WriteLine(string.Join(", ", bestSolutionItems));
+        Console.ResetColor();
+
+        Console.ReadLine();
+    }
+
+    private static void InputTheProductsDataIntoArrays(Dictionary<string, List<int>> allProducts, int[,] valuesArray, int[,] keepArray)
+    {
+        for (int r = 1; r < valuesArray.GetLength(0); r++)
+        {
+            var currentProduct = allProducts.ElementAt(r - 1);
+            int currentProductWeight = currentProduct.Value[0];
+            int currentProductCost = currentProduct.Value[1];
+
+            for (int c = 1; c < valuesArray.GetLength(1); c++)
+            {
+                if (currentProductWeight == c)
+                {
+                    if (valuesArray[r - 1, c] <= currentProductCost)
+                    {
+                        valuesArray[r, c] = currentProductCost;
+                        keepArray[r, c] = 1;
+                    }
+                    else
+                    {
+                        valuesArray[r, c] = valuesArray[r - 1, c];
+                    }
+                }
+                else if (currentProductWeight < c)
+                {
+                    int remainingSpace = c - currentProductWeight;
+                    int secondItemCost = valuesArray[r - 1, remainingSpace];
+
+                    int currentCostPlusOneMoreItem = currentProductCost + secondItemCost;
+
+                    if (currentCostPlusOneMoreItem >= valuesArray[r - 1, c])
+                    {
+                        valuesArray[r, c] = currentCostPlusOneMoreItem;
+                        keepArray[r, c] = 1;
+                    }
+                    else
+                    {
+                        valuesArray[r, c] = valuesArray[r - 1, c];
+                    }
+                }
+            }
+        }
+    }
+
+    private static void FindBestSolutionItems(int knapsackSize, Dictionary<string, List<int>> allProducts, int[,] keepArray, List<string> bestSolutionItems)
+    {
+        bool traceContinue = true;
+        int currentRow = keepArray.GetLength(0) - 1;
+        int currentCol = keepArray.GetLength(1) - 1;
+        int currentKnapsackWeight = 0;
+
+        while (traceContinue)
+        {
+            int currentCell = keepArray[currentRow, currentCol];
+
+            if (currentCell == 1)
+            {
+                var currentBestSolutionItem = allProducts.ElementAt(currentRow - 1);
+
+                if ((currentKnapsackWeight + currentBestSolutionItem.Value[0]) <= knapsackSize)
+                {
+                    bestSolutionItems.Add(currentBestSolutionItem.Key);
+                    currentKnapsackWeight += currentBestSolutionItem.Value[0];
+                }
+                else
+                {
+                    traceContinue = false;
+                }
+
+                currentRow--;
+                currentCol--;
+            }
+            else
+            {
+                currentRow--;
+            }
+
+            if (currentRow == 0)
+            {
+                traceContinue = false;
+            }
+        }
+    }
+
+    private static void GetAllInputedProductsFromConsole(int productsCount, Dictionary<string, List<int>> allProducts)
+    {
+        for (int i = 0; i < productsCount; i++)
+        {
+            Console.Write("Enter product " + (i + 1) + ": ");
+            string inputProduct = Console.ReadLine();
+            var arrOfProductValues = inputProduct.Split(new string[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+
+            string prodctName = default(string);
+            int productWeight = default(int);
+            int productCost = default(int);
+
+            // Checking is the product are correct.
+            while (arrOfProductValues.Length < 3 ||
+                (arrOfProductValues[0].Length == 0 ||
+                (!int.TryParse(arrOfProductValues[1], out productWeight) || productWeight < 1) ||
+                (!int.TryParse(arrOfProductValues[2], out productCost) || productCost < 1)))
+            {
+                Console.Write("ReEnter product " + (i + 1) + ": ");
+                inputProduct = Console.ReadLine();
+                arrOfProductValues = inputProduct.Split(new string[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            // Adding the product into Dictionary.
+            prodctName = arrOfProductValues[0];
+            productWeight = int.Parse(arrOfProductValues[1]);
+            productCost = int.Parse(arrOfProductValues[2]);
+
+            allProducts.Add(prodctName, new List<int> { productWeight, productCost });
+        }
+    }
+
+    private static void ValidateTheInputProductsCount(ref string consoleInputProductsCount, ref int productsCount)
+    {
+        do
+        {
+            Console.Write("Enter the count of products: ");
+            consoleInputProductsCount = Console.ReadLine();
+        }
+        while (!int.TryParse(consoleInputProductsCount, out productsCount) || productsCount < 1);
+    }
+
+    private static void ValidateTheInputKnapsackSize(ref string consoleInputKnapsackSize, ref int knapsackSize)
+    {
+        do
+        {
+            Console.Write("Enter the size of Knapsack: ");
+            consoleInputKnapsackSize = Console.ReadLine();
+        }
+        while (!int.TryParse(consoleInputKnapsackSize, out knapsackSize) || knapsackSize < 1);
+    }
+}
---
+++ 01. Programming/05. C# Data Structures and Algorithms/10. Dynamic Programming/02. Minimum Edit Distance/LevenshteinDistanceSolution.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class LevenshteinDistanceSolution
+{
+    private const decimal CostDelete = 0.9M;
+    private const decimal CostInsert = 0.8M;
+    private const decimal CostReplace = 1M;
+
+    public static void Main()
+    {
+        var result1 = Compute("developer", "enveloped");
+        Console.WriteLine("Words: developer -> enveloped");
+        Console.WriteLine("Distance = {0}", result1);
+        Console.WriteLine();
+
+        var result2 = Compute("developer", "eveloper");
+        Console.WriteLine("Words: developer -> eveloper");
+        Console.WriteLine("Distance = {0}", result2);
+        Console.WriteLine();
+
+        var result3 = Compute("eveloper", "enveloper");
+        Console.WriteLine("Words: eveloper -> enveloper");
+        Console.WriteLine("Distance = {0}", result3);
+        Console.WriteLine();
+
+        var result4 = Compute("eveloper", "");
+        Console.WriteLine("Words: eveloper ->  ");
+        Console.WriteLine("Distance = {0}", result4);
+        Console.WriteLine();
+
+        var result5 = Compute("", "eveloper");
+        Console.WriteLine("Words:  -> enveloper");
+        Console.WriteLine("Distance = {0}", result5);
+        Console.WriteLine();
+
+        var result6 = Compute("", "");
+        Console.WriteLine("Words:  -> ");
+        Console.WriteLine("Distance = {0}", result6);
+    }
+
+    /// <summary>
+    /// Compute the distance between two words.
+    /// </summary>
+    public static decimal Compute(string word1, string word2)
+    {
+        decimal[,] table;
+
+        int n = word1.Length;
+        int m = word2.Length;
+        table = new decimal[n + 1, m + 1];
+
+        // Step 1: Fill cost of deletions
+        for (int row = 0; row <= n; row++)
+        {
+            table[row, 0] = row * CostDelete;
+        }
+
+        // Step 2: Fill cost of insertions
+        for (int col = 0; col <= m; col++)
+        {
+            table[0, col] = col * CostInsert;
+        }
+
+        // Step 3: Calculate and choose min cost operation
+        for (int row = 1; row <= n; row++)
+        {
+            // Step 4
+            for (int col = 1; col <= m; col++)
+            {
+                // Step 5: Calculate the cost of replacing. If the letters are equal it is 0, otherwise its the replace operation cost
+                decimal cost = (word2[col - 1] == word1[row - 1]) ? 0 : CostReplace;
+
+                // Step 6: Find the minimal cost operation of the 3 possible
+                decimal delete = table[row - 1, col] + CostDelete;
+                decimal replace = table[row - 1, col - 1] + cost;
+                decimal insert = table[row, col - 1] + CostInsert;
+
+                table[row, col] = Math.Min(
+                    Math.Min(delete, insert),
+                    replace);
+            }
+        }
+
+        // Step 7: Take and return the result (most down-right cell)
+        return table[n, m];
+    }
+}
