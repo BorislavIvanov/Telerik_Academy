a88cc5dde02c81de164a99a999440b65aafbb821
BorislavIvanov <borislav.a.ivanov@gmail.com>
2014-11-09 14:14:09 +0200

    Added new Web Forms HW - Introduction to ASP.NET

---
+++ 04. Software Technologies/ASP.NET Web Forms/01. Introduction to ASP.NET/01. Calculator - ASP.NET Web Forms/Calculator.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="_01.Calculator___ASP.NET_Web_Forms.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator - Web Forms</title>
+    <style type="text/css">
+    </style>
+</head>
+<body>
+    <form id="calculatorForm" runat="server">
+        <div>
+            <asp:Label ID="lblFirstNumber" runat="server">FirstNumber:</asp:Label>
+            <asp:TextBox ID="tbFirstNumber" runat="server"></asp:TextBox>
+        </div>
+        <br />
+        <div>
+            <asp:Label ID="lblSecondNumber" runat="server">SecondNumber:</asp:Label>
+            <asp:TextBox ID="tbSecondNumber" runat="server"></asp:TextBox>
+        </div>
+        <br />
+        <asp:Button ID="submitButton" runat="server" Text="Calculate" OnClick="submitButton_Click"/>
+    </form>
+    <hr />
+    <asp:Label ID="lblResult" runat="server">Result:</asp:Label>
+    <asp:Literal ID="litResult" runat="server"></asp:Literal>
+</body>
+</html>
---
+++ 04. Software Technologies/ASP.NET Web Forms/01. Introduction to ASP.NET/01. Calculator - ASP.NET Web Forms/Calculator.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01.Calculator___ASP.NET_Web_Forms
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            string numberOneInput = tbFirstNumber.Text;
+            int numberOne = default(int);
+
+            bool numberOneIsNumberic = int.TryParse(numberOneInput, out numberOne);
+
+            if (!numberOneIsNumberic || String.IsNullOrEmpty(numberOneInput))
+            {
+                litResult.Text = "Incorrect First Number!!!";
+                return;
+            }
+
+            string numberTwoInput = tbSecondNumber.Text;
+            int numberTwo = default(int);
+
+            bool numberTwoIsNumberic = int.TryParse(numberTwoInput, out numberTwo);
+
+            if (!numberTwoIsNumberic || String.IsNullOrEmpty(numberTwoInput))
+            {
+                litResult.Text = "Incorrect Second Number!!!";
+                return;
+            }
+
+            litResult.Text = (numberOne + numberTwo).ToString();
+        }
+    }
+}
